597faa4a606a322eb3b169f4c2a8b9782154fec0
zeel-codder <zeelprajapati321@gmail.com>
2023-10-10 17:36:56 +0530

    feat: Add Appointment Group, User Appointment Availability, Appointment Time Slot, and Members DocTypes in the Frappe Appointment Module

---
+++ frappe_appointment/frappe_appointment/doctype/members/members.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, rtCamp and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Members(Document):
+	pass
---
+++ frappe_appointment/frappe_appointment/doctype/user_appointment_availability/test_user_appointment_availability.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, rtCamp and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestUserAppointmentAvailability(FrappeTestCase):
+	pass
