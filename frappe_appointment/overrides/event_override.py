d52a00fd10539645cbf2ed96b8ea701332b6fccc
zeel-codder <zeelprajapati321@gmail.com>
2023-10-26 16:51:36 +0530

    refactor: added documentation in helpers and overrides files

--- frappe_appointment/overrides/event_override.py
+++ frappe_appointment/overrides/event_override.py
@@ -1,9 +1,12 @@
 import frappe
-from frappe import _
 import datetime
+import requests
+import json
+
+from frappe import _
 from frappe.desk.doctype.event.event import Event
 from frappe.integrations.doctype.google_calendar.google_calendar import (
-	get_google_calendar_object
+	get_google_calendar_object,
 )
 from frappe.utils import (
 	get_datetime,
@@ -19,12 +22,20 @@ from frappe_appointment.constants import (
 from frappe_appointment.frappe_appointment.doctype.appointment_group.appointment_group import (
 	vaild_date,
 )
-import requests
-import json
+from frappe_appointment.helpers import utc_to_sys_time
 
 
 class EventOverride(Event):
+	"""Event Doctype Overwrite
+
+	Args:
+		Event (class): Default class
+	"""
 	def before_insert(self):
+		"""Handle the Appointment Group in Event
+		"""
+		super().before_insert()
+
 		if self.custom_appointment_group:
 			self.appointment_group = frappe.get_doc(
 				APPOINTMENT_GROUP, self.custom_appointment_group
@@ -33,6 +44,8 @@ class EventOverride(Event):
 			self.update_attendees_for_appointment_group()
 
 	def send_meet_email(self):
+		"""Sent the meeting link email to the given user using the provided Email Template
+		"""
 		appointment_group = self.appointment_group
 
 		if (
@@ -45,7 +58,9 @@ class EventOverride(Event):
 				appointment_group=self.appointment_group, event=self, metadata=self.event_info
 			)
 
+			# Only send the email to first user of custom_doctype_link_with_event
 			send_doc_value = self.custom_doctype_link_with_event[0]
+
 			send_doc = frappe.get_doc(
 				send_doc_value.reference_doctype, send_doc_value.reference_docname
 			)
@@ -58,18 +73,26 @@ class EventOverride(Event):
 			)
 
 	def get_recipients_event(self):
+		"""Get the list of recipients as per event_participants 
+
+		Returns:
+			list: recipients emails
+		"""
 		if not self.event_participants:
 			return []
 
 		recipients = []
 
 		for participant in self.event_participants:
+			# Don't send the meet link to Appointment Group Members
 			if participant.reference_doctype != USER_APPOINTMENT_AVAILABILITY:
 				recipients.append(participant.email)
 
 		return recipients
 
 	def update_attendees_for_appointment_group(self):
+		"""Insert Appointment Group Member as Event participants
+		"""
 		members = self.appointment_group.members
 
 		for member in members:
@@ -91,7 +114,15 @@ class EventOverride(Event):
 				pass
 
 	def handle_webhook(self, body):
+		"""Handle the webhook call
+
+		Args:
+			body (object): data the send in req body
+		"""
+  
 		def datetime_serializer(obj):
+			"""Handle the encode datetime object in JSON
+			"""
 			if isinstance(obj, datetime.datetime):
 				return obj.isoformat()
 
@@ -113,6 +144,7 @@ class EventOverride(Event):
 		except Exception as e:
 			return False
 
+
 @frappe.whitelist(allow_guest=True)
 def create_event_for_appointment_group(
 	appointment_group_id: str,
@@ -122,11 +154,27 @@ def create_event_for_appointment_group(
 	event_participants,
 	**args,
 ):
+	"""API Endpoint to Create the Event
+
+	Args:
+		appointment_group_id (str): Appointment ID
+		date (str): Date for which the event is scheduled
+		start_time (str): Start time of the event
+		end_time (str): End time of the event
+		event_participants (list): List of participants
+		args (object): Query Parameters of api
+
+	Returns:
+		res (object): Result object
+	"""
+    #query parameters
 	event_info = args
-	starts_on =utc_to_sys_time(start_time)
+ 
+	starts_on = utc_to_sys_time(start_time)
 	ends_on = utc_to_sys_time(end_time)
-	reschedule= event_info.get('reschedule',False)
-		
+
+	reschedule = event_info.get("reschedule", False)
+
 	appointment_group = frappe.get_last_doc(
 		APPOINTMENT_GROUP, filters={"route": appointment_group_id}
 	)
@@ -142,17 +190,21 @@ def create_event_for_appointment_group(
 	if len(members) <= 0:
 		return frappe.throw(_("No Member found"))
 
+
 	google_calendar = frappe.get_last_doc(
 		doctype="Google Calendar", filters={"user": members[0].user}
 	)
 
 	google_calendar_api_obj, account = get_google_calendar_object(google_calendar.name)
+	
 	if reschedule:
-		event = frappe.get_last_doc('Event',filters={'custom_appointment_group':appointment_group.name})
+		event = frappe.get_last_doc(
+			"Event", filters={"custom_appointment_group": appointment_group.name}
+		)
 		event.starts_on = starts_on
-		event.ends_on =ends_on	
+		event.ends_on = ends_on
 		event.save(ignore_permissions=True)
-		
+
 		if not event.handle_webhook(
 			{
 				"event": event.as_dict(),
@@ -162,6 +214,7 @@ def create_event_for_appointment_group(
 		):
 			return frappe.throw(_("Unable to Update an event"))
 		return frappe.msgprint("Interview has been Re-Scheduled.")
+
 	calendar_event = {
 		"doctype": "Event",
 		"subject": event_info.get("subject"),
@@ -198,11 +251,3 @@ def create_event_for_appointment_group(
 	frappe.db.commit()
 
 	return _("Event has been created")
-
-
-def utc_to_sys_time(time:str):
-	return get_datetime_str(
-		convert_utc_to_system_timezone(
-			datetime.datetime.fromisoformat(time).replace(tzinfo=None)
-		)
-	)
--- frappe_appointment/overrides/google_calendar_override.py
+++ frappe_appointment/overrides/google_calendar_override.py
@@ -2,7 +2,10 @@ from frappe.integrations.doctype.google_calendar.google_calendar import (
 	GoogleCalendar,
 )
 
-
 class GoogleCalendarOverride(GoogleCalendar):
+	"""Google Calendar DocType overwrite"""
+
 	def before_insert(self):
+		"""Set the google_calendar_id as the user's email. So, for each user, the primary calendar will be the one used to update, get, and insert events."""
+		super().before_insert()
 		self.google_calendar_id = self.user
