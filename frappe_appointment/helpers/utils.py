0369397cb9433417becfbfd77b9384937916096c
zeel-codder <zeelprajapati321@gmail.com>
2024-04-12 15:04:29 +0530

    fix: update algorithm slot sort function and update group name in frontend

--- frappe_appointment/helpers/utils.py
+++ frappe_appointment/helpers/utils.py
@@ -1,10 +1,14 @@
-import datetime
+from datetime import datetime
 from frappe.utils import (
 	get_datetime,
 	convert_utc_to_system_timezone,
 	get_datetime_str,
 )
+
+from frappe.utils.data import get_date_str, get_system_timezone
 import pytz
+
+from dateutil import parser
 
 weekdays = [
 	"Monday",
@@ -15,6 +19,69 @@ weekdays = [
 	"Saturday",
 	"Sunday",
 ]
+
+def get_today_min_max_time(date: datetime):
+    """Retrieve the current day's start and end time in UTC format.
+
+    Args:
+    date (datetime): date
+
+    Returns:
+    list: Date start and end time
+    """
+    time_min = datetime(date.year, date.month, date.day, 0, 0, 0)
+    time_max = datetime(date.year, date.month, date.day, 23, 59, 59)
+
+    time_min_str = time_min.isoformat() + "Z"
+    time_max_str = time_max.isoformat() + "Z"
+
+    return [time_max_str, time_min_str]
+
+
+def get_utc_datatime_with_time(date: datetime, time: str) -> datetime:
+    """Function to generate a datetime object for a given date and time.
+
+    Args:
+    date (datetime): Date
+    time (str): Time
+
+    Returns:
+    datetime: Updated datetime object
+    """
+    system_timezone = pytz.timezone(get_system_timezone())
+    local_datetime = system_timezone.localize(
+        datetime.strptime(f"{get_date_str(date)} {time}", "%Y-%m-%d %H:%M:%S")
+    )
+    return local_datetime.astimezone(pytz.utc)
+
+
+def convert_timezone_to_utc(date_time: str, time_zone: str) -> datetime:
+    """Helper function to convert a given datetime string to a datetime object with the specified time zone.
+
+    Args:
+    date_time (str): Datetime string
+    time_zone (str): Time zone
+
+    Returns:
+    datetime: Datetime object
+    """
+    local_datetime = parser.parse(date_time).astimezone(pytz.timezone(time_zone))
+    return local_datetime.astimezone(pytz.utc)
+
+
+def convert_datetime_to_utc(date_time: datetime) -> datetime:
+    """Converts the given datetime object to a UTC timezone datetime object.
+
+    Args:
+    date_time (datetime): Datetime Object
+
+    Returns:
+    datetime: Updated Object
+    """
+    system_timezone = pytz.timezone(get_system_timezone())
+    local_datetime = system_timezone.localize(date_time)
+    return local_datetime.astimezone(pytz.utc)
+
 
 
 def get_weekday(date_time: datetime) -> str:
@@ -37,3 +104,36 @@ def utc_to_given_time_zone(utc_datetime: datetime, time_zone_offset:str) -> str:
 	converted_datetime = utc_datetime.astimezone(pytz.FixedOffset(int(time_zone_offset)))
  
 	return converted_datetime
+
+
+def compare_end_time_slots(current_slot, next_slot):
+    
+	current_slot, next_slot = get_time_slots_utc(current_slot) ,  get_time_slots_utc(next_slot)
+
+	if current_slot["start_time"] != next_slot["start_time"]:
+		return cmp_items(current_slot["start_time"], next_slot["start_time"])
+	
+	return cmp_items(current_slot["end_time"], next_slot["end_time"])
+
+
+def get_time_slots_utc(slot):
+    return {
+		"start_time": get_datetime_str(
+            convert_timezone_to_utc(
+                slot["start"]["dateTime"], slot["start"]["timeZone"]
+            )
+        ),
+		"end_time": get_datetime_str(
+            convert_timezone_to_utc(
+                slot["end"]["dateTime"], slot["end"]["timeZone"]
+            )
+        ),
+	}
+
+def cmp_items(a, b):
+    if a > b:
+        return 1
+    elif a == b:
+        return 0
+    else:
+        return -1
