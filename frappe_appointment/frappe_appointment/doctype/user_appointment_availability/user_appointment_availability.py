26e2f2df32f0983a67f00cce238ae3cf70ae3fd1
Shivam Saini <shivamsn97@gmail.com>
2025-02-16 19:30:44 +0000

    Appointment Availability: Add validations for unique weekdays and valid timeslots

--- frappe_appointment/frappe_appointment/doctype/user_appointment_availability/user_appointment_availability.py
+++ frappe_appointment/frappe_appointment/doctype/user_appointment_availability/user_appointment_availability.py
@@ -23,6 +23,19 @@ SLUG_REGEX = re.compile(r"^[a-z0-9]+(?:-[a-z0-9]+)*$")
 
 class UserAppointmentAvailability(Document):
     def validate(self):
+        # validate time slots, so that start time is less than end time, and weekdays are unique
+        if self.appointment_time_slot:
+            weekdays = []
+            for slot in self.appointment_time_slot:
+                if slot.start_time > slot.end_time:
+                    frappe.throw(frappe._("Start time should be less than end time for the day {0}").format(slot.day))
+                if slot.day in weekdays:
+                    frappe.throw(
+                        frappe._("Day {0} is repeated in the time slots. Make sure each day is unique.").format(
+                            slot.day
+                        )
+                    )
+                weekdays.append(slot.day)
         calendar = frappe.get_doc("Google Calendar", self.google_calendar)
         if not calendar.custom_is_google_calendar_authorized:
             frappe.throw(frappe._("Please authorize Google Calendar before creating appointment availability."))
