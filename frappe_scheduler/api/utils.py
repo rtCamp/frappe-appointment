a7d6dab9026e0b7a5bde4d4b099384354141731f
Shivam Saini <shivamsn97@gmail.com>
2025-02-24 10:35:58 +0000

    refactor: Replace varable g_calendars with google_calendars

--- frappe_scheduler/api/utils.py
+++ frappe_scheduler/api/utils.py
@@ -10,24 +10,24 @@ def check_google_calendar_setup(user):
         frappe.clear_last_message()
 
     is_google_calendar_setup = False
-    g_calendars = frappe.get_all(
+    google_calendars = frappe.get_all(
         "Google Calendar",
         filters={"user": user},
         fields=["name", "enable", "custom_is_google_calendar_authorized"],
     )
 
-    if len(g_calendars) > 0:
+    if len(google_calendars) > 0:
         is_google_calendar_setup = True
 
     google_calendar_id = None
-    if len(g_calendars) == 1:
-        google_calendar_id = g_calendars[0].name
+    if len(google_calendars) == 1:
+        google_calendar_id = google_calendars[0].name
 
     is_google_calendar_enabled = True
-    if not any([g_calendar.enable for g_calendar in g_calendars]):
+    if not any([g_calendar.enable for g_calendar in google_calendars]):
         is_google_calendar_enabled = False
     is_google_calendar_authorized = True
-    if not any([g_calendar.custom_is_google_calendar_authorized for g_calendar in g_calendars]):
+    if not any([g_calendar.custom_is_google_calendar_authorized for g_calendar in google_calendars]):
         is_google_calendar_authorized = False
 
     return {
