a79262ce550d0caf3a85e175f8b2341cbfccdd57
Shivam Saini <shivamsn97@gmail.com>
2025-03-21 17:18:23 +0530

    Change after_install to after_sync for ERPNext fields setup and email template import

--- frappe_appointment/hooks.py
+++ frappe_appointment/hooks.py
@@ -26,7 +26,7 @@ app_include_js = [
 after_migrate = "frappe_appointment.tasks.setup_erpnext_fields.setup_erpnext_fields"
 
 before_install = "frappe_appointment.tasks.import_email_templates.import_email_templates"
-after_install = ["frappe_appointment.tasks.setup_erpnext_fields.setup_erpnext_fields", "frappe_appointment.tasks.import_form_tour_google_calendar.import_doc"]
+after_sync = ["frappe_appointment.tasks.setup_erpnext_fields.setup_erpnext_fields", "frappe_appointment.tasks.import_form_tour_google_calendar.import_doc"]
 
 # include js, css files in header of web template
 # web_include_css = "/assets/frappe_appointment/css/frappe_appointment.css"
