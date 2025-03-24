d942486f52bcfe2c15cc7c8bce6aaa13a88a0765
Samarth Upare <samarth.upare@redtra.com>
2025-03-24 23:05:54 +0530

    fix: links import error before_install for email templates

--- frappe_appointment/tasks/import_email_templates.py
+++ frappe_appointment/tasks/import_email_templates.py
@@ -40,3 +40,4 @@ def import_email_templates():
     for email_template in EMAIL_TEMPLATES:
         frappe.get_doc(email_template).insert(ignore_permissions=True, ignore_if_duplicate=True, ignore_links=True)
     print("Email Templates Imported")
+ 
