0ca20b26001df5a77db26cc6dee5d9b33f9f850c
Shivam Saini <shivamsn97@gmail.com>
2025-04-21 07:18:09 +0000

    Fix appointment group duration update patch

--- frappe_appointment/patches/v0_1/change_fieldtype_to_duration.py
+++ frappe_appointment/patches/v0_1/change_fieldtype_to_duration.py
@@ -3,6 +3,7 @@ import frappe
 
 def execute():
     appointment_groups = frappe.get_all("Appointment Group")
+    appointment_group_docs = {}
 
     for appointment_group in appointment_groups:
         appointment_group_doc = frappe.get_doc("Appointment Group", appointment_group.name)
@@ -11,4 +12,15 @@ def execute():
 
         appointment_group_doc.minimum_buffer_time = minimum_buffer_time
         appointment_group_doc.duration_for_event = duration_for_event
-        appointment_group_doc.db_update()
+        appointment_group_docs[appointment_group.name] = {
+            "minimum_buffer_time": minimum_buffer_time,
+            "duration_for_event": duration_for_event,
+        }
+
+    frappe.reload_doc("Frappe Appointment", "doctype", "Appointment Group")
+
+    for appointment_group_doc in appointment_group_docs:
+        appointment_group = frappe.get_doc("Appointment Group", appointment_group_doc)
+        appointment_group.minimum_buffer_time = appointment_group_docs[appointment_group_doc]["minimum_buffer_time"]
+        appointment_group.duration_for_event = appointment_group_docs[appointment_group_doc]["duration_for_event"]
+        appointment_group.save()
