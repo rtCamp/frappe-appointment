1eb4a16fb7c0dfcec5c16053d44f0c0b74108e39
Harish Chandran <harish3304.work@gmail.com>
2026-07-27 20:51:34 +0530

    chore: narrow the multitenancy semgrep suppression to its rule id

--- frappe_appointment/helpers/email.py
+++ frappe_appointment/helpers/email.py
@@ -58,7 +58,7 @@ def send_email_template_mail(doc, args, email_template, recipients=None, attachm
 
         for attachment in attachments:
             # _file is function-local here, not a module-level global; the multitenancy rule is a false positive
-            # nosemgrep
+            # nosemgrep: frappe-breaks-multitenancy
             _file = frappe.get_doc("File", attachment["fid"], ignore_permissions=True)
             fcontent = _file.get_content()
             new_attachments.append({"fname": _file.file_name, "fcontent": fcontent})
